4c3c11e7a3baec5d5a9110de528248b85ca26861
Sphen <Lpanda909@gmail.com>
2022-09-28 11:42:23 -0700

    Finished

---
+++ src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+    private String name;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int toss(Dice sides) {
+        sides.roll();
+        score = sides.view();
+        return score;
+    }
+
+    public Player(String n) {
+        name = n;
+    }
+}
