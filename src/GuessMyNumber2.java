f7322f5015527ee5667513cc807940dfcda6509b
Juanc <Juancervantesmrz69@gmail.com>
2024-02-24 19:18:25 -0800

    Completed Lab 005

--- src/GuessMyNumber2.java
+++ src/GuessMyNumber2.java
@@ -1,5 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessMyNumber2 {
     public static void main(String[] args) {
+        System.out.println("Welcome to Guess My Number Game Part 2");
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(100) + 1;
+        int attempts = 1;
+        int maxAttempts = 3;
 
+        System.out.print("Guess a number between 1 and 100: ");
+        int guess = scanner.nextInt();
+        System.out.println("The number is: " + guess);
+
+        if (guess == number) {
+            System.out.println("Congrats! You guessed the number.");
+        } else {
+            while (attempts < maxAttempts) {
+                if (guess < number) {
+                    System.out.println("Too low. Try again.");
+                } else {
+                    System.out.println("Too high. Try again.");
+                }
+
+                System.out.print("Guess a number between 1 and 100: ");
+                guess = scanner.nextInt();
+                System.out.println("The number is: " + guess);
+
+                attempts++;
+
+                if (guess == number) {
+                    System.out.println("Congrats! You guessed the number.");
+                    break;
+                }
+            }
+
+            if (attempts == maxAttempts) {
+                System.out.println("You have no more attempts remaining. The correct number was: " + number);
+            }
+        }
     }
 }
+
+
+
+
+
+
