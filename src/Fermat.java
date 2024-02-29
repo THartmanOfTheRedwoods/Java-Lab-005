3892b7281b0f5cbfbb8508e4a48525ab75d871f5
kieran <kierancooke1111@gmail.com>
2024-02-29 14:38:11 -0800

    lab5

--- src/Fermat.java
+++ src/Fermat.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Fermat { 
+public class Fermat {  
     public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in);
 
--- src/Rewrite.java
+++ src/Rewrite.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Rewrite {  
+public class Rewrite {   
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Please input a number for x: ");
