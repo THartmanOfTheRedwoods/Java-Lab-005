a40125ff47451dd326ad088a8f18522607ddba89
kieran <kierancooke1111@gmail.com>
2024-02-29 14:29:46 -0800

    lab5

--- src/GuessMyNumber2.java
+++ src/GuessMyNumber2.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class GuessMyNumber2 {
+public class GuessMyNumber2 { 
     public static void main(String[] args) {
         Random random = new Random();
         int number = random.nextInt(100) + 1;
